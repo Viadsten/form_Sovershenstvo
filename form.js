62649d7c6c84a857353742187837885b338de66d
Viadsten <stanulevich.vg@yandex.ru>
2020-11-20 16:11:06 +0300

    client_update

--- form.js
+++ form.js
@@ -45,18 +45,22 @@ function nextPrev(n) {
   else{
     btnWrp.css('margin-top', '180px');
   }
-  if ((currentTab == 3)&&(window.matchMedia('(max-width: 550px)').matches)){
-    btnWrp.css('margin-top', '150px');
+  if ((currentTab == 3)&&(window.matchMedia('(min-width: 50px)').matches)){
+    btnWrp.css('margin-top', '160px');
     }
   if ((currentTab == 3)&&(window.matchMedia('(min-width: 850px)').matches)){
-    x[currentTab].classList.add("color-bg");
-    btnWrp.css('margin-top', '40px');
-    }
+    regForm[0].classList.add("color-bg");
+    btnWrp.css('margin-top', '90px');
+    }else{ regForm[0].classList.remove("color-bg");}
+    if ((currentTab == 3)&&(window.matchMedia('(min-width: 950px)').matches)){
+      btnWrp.css('margin-top', '-49px');
+      }
     if ((currentTab == 4)&&(window.matchMedia('(max-width: 800px)').matches)){
       btnWrp.css('margin-top', '30px');
       }
+
     if ((currentTab == 4)&&(window.matchMedia('(min-width: 800px)').matches)){
-      btnWrp.css('margin-top', '50px');
+      btnWrp.css('margin-top', '-62px');
       }
       if ((currentTab == x.length - 1)&&(window.matchMedia('(max-width: 700px)').matches)){
         btnWrp.css('margin-top', '210px');
@@ -182,11 +186,23 @@ $(document).ready(function(){
 
 
 var checkbox = $(".color-check--label");
-var checkboxColor =       ['#f5f5dc', '#30d5c8', '#9b2d30', '#42aaff', '#ffff00', '#008000', '#ffd700', '#964b00', '#ff0000', '#dc143c', '#ffa500', '#ffe5b4', '#ffc0cb', '#c0c0c0', '#808080', '#0000ff', '#c8a2c8', '#8b00ff', '#000000', '#808000', '#9acd32', '#7fffd4', '#884535', '#ffffff'];
-var checkboxBorderColor = ['#f5f5dc', '#99eae3', '#cc9696', '#a0d4ff', '#f7f9a0', '#7fbf7f', '#ffeb7f', '#caa57f', '#fc8584', '#ee889c', '#ffd27f', '#fef2d9', '#ffdfe5', '#dfdfdf', '#bfbfbf', '#8a87fa', '#e3d0e4', '#c57fff', '#777169', '#c0c083', '#cce699', '#befce9', '#d6bfb9', '#bfbfbf'];
+var checkboxColor =       ['#f5f5dc', '#30d5c8', '#9b2d30', '#42aaff', '#ffff00', '#008000', '      ' , '#ffd700', '#964b00', '#ff0000', '#dc143c', '#ffa500', '#ffe5b4', '       ', '#ffc0cb', '#c0c0c0', '#808080', '#0000ff', '#c8a2c8', '#8b00ff', '#002137', '#000000', '#808000', '#9acd32', '#7fffd4', '#884535', '#ffffff', '#cd5b45'];
+var checkboxBorderColor = ['#f6f6df', '#99eae3', '#cc9696', '#a0d4ff', '#f7f9a0', '#7fbf7f', '#b4a99d', '#ffeb7f', '#caa57f', '#fc8584', '#ee889c', '#ffd27f', '#fef2d9', '#7f7f7f', '#ffdfe5', '#dfdfdf', '#bfbfbf', '#8a87fa', '#e3d0e4', '#c57fff', '#7f909b', '#777169', '#c0c083', '#cce699', '#befce9', '#d6bfb9', '#bfbfbf', '#e2aaa0'];
+
 
 for (i=0; i < checkbox.length; i++){
-  checkbox[i].style.backgroundColor = checkboxColor[i];
+  if (i == 6){
+    checkbox[i].style.background = "url('img/multicolor.png')";
+    checkbox[i].style.backgroundPosition="center";
+    checkbox[i].style.borderColor = checkboxBorderColor[i];
+    i++;
+  }else if(i == 13){
+    checkbox[i].style.background = "url('img/BandW.png')";
+    checkbox[i].style.backgroundPosition="center";
+    checkbox[i].style.borderColor = checkboxBorderColor[i];
+    i++;
+  }
+  checkbox[i].style.background = checkboxColor[i];
   checkbox[i].style.borderColor = checkboxBorderColor[i];
 }
 
